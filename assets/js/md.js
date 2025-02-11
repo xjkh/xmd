8800836c873c3759a0e96ae024f8425a8b2b686f
xjkh <xjkh@xprodx.com>
2025-02-10 16:50:08 -0800

    Update md.js

--- assets/js/md.js
+++ assets/js/md.js
@@ -4,7 +4,6 @@ function initMD() {
     var nav_menu_toggle = document.querySelector("header button");
     nav_menu_toggle.addEventListener("click", function() {
         body.classList.toggle("md-no-scroll");
-        //this.toggleAttribute("aria-expanded", "true");
         this.ariaExpanded = this.ariaExpanded !== 'true';
     });
 
